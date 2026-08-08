d76382d62047fd1363676a8e24a2f0538a92b205
Ahmed Fadhil <ahmed99pro@gmail.com>
2026-08-08 19:25:16 +0300

    Restore design page

---
+++ noteera-react/src/pages/design.jsx
@@ -0,0 +1,10 @@
+function Design() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Design Page</h1>
+      <p>هذه صفحة التصميم مؤقتًا.</p>
+    </div>
+  );
+}
+
+export default Design;
